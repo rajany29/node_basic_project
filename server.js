3b00c5bfa57760c2a7550b1867b7105d8d7ec134
rajany29 <rajany296@gmail.com>
2025-02-22 18:15:47 +0530

    new_changes

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ app.use("/test", (req, res) => {
   res.send("Hello from testing!");
 });
 
+app.use("/api", (req, res) => {
+  res.send("Hello from api testing!");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port : ${PORT}`));
