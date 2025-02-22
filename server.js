250f4fc61333d61ff8fb0bf3fe9a151cd4a9695e
rajany29 <rajany296@gmail.com>
2025-02-22 18:04:37 +0530

    'changes'

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from CI/CD pipeline!");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from testing!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App running on port : ${PORT}`));
